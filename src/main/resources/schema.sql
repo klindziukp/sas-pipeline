df3dcc3658674590ab5308fa02c44c4c5f1dd360
Pavel_Klindziuk <pavel_klindziuk@epam.com>
2022-11-06 14:58:45 +0300

    SAS-PIPELINE: Add sonar integration

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,2 @@
-create table player(id serial primary key, name varchar(255) not null, age number, club varchar(255) not null, nationality varchar(255) not null)
+DROP TABLE IF EXISTS player
+CREATE TABLE player(id serial primary key, name varchar(255) not null, age number, club varchar(255) not null, nationality varchar(255) not null)
